1026d06147b0ca362500bae385f91d3ff5e28f62
guilh <guilherme.h.p.cravo@gmail.com>
2025-06-02 23:51:38 -0300

    ATUALIZAÇÃO02.06

--- src/main/java/br/unipar/programacaoweb/livraria/configuration/SecurityConfig.java
+++ src/main/java/br/unipar/programacaoweb/livraria/configuration/SecurityConfig.java
@@ -69,7 +69,6 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .build();
     }
-
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         String[] origins = corsOrigin.split(",");
